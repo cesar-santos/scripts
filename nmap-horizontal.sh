92f03f0e6cb7734a92f41071681c1d772306d2c8
César Santos <cesar-santos@users.noreply.github.com>
2019-05-05 18:43:21 -0300

    Fix command syntax to make it work
    Previous version was nonfunctional due to syntax mistakes. This version not only corrects it, as well add more comments to make it understandable.

--- nmap-horizontal.sh
+++ nmap-horizontal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Constants
-DATE=`$(date +%s)`
-FOLDER="nmap-horizontal-logs"
+DATE=$(date +%s)
+FOLDER="nmap_logs"
 
 # Arguments
 ip_range=""
@@ -34,7 +34,7 @@ sanitize()
     oct1=$(echo $ip_range | tr "." " " | awk '{print $1}')
     oct2=$(echo $ip_range | tr "." " " | awk '{print $2}')
     oct3=$(echo $ip_range | tr "." " " | awk '{print $3}')
- 
+
     if [[ $oct1 -gt 0 && $oct1 -lt 256 ]]
     then
         if [[ $oct2 -gt 0 && $oct2 -lt 256 ]]
@@ -56,85 +56,6 @@ sanitize()
     fi
 }
 
-# Verify if inputs were inserted correctly
-check-arguments()
-{
-	# If no inputs, help user to use it :)
-	if [ $# -eq 0 ]
-	then
-		help
-		exit
-	fi
-	
-	# Save first argument and test it
-	ip_range=$1
-	shift
-	sanitize
-
-	# Verify if there are extra arguments
-	for i in {1..3}
-	do
-		if [ $# -eq 0 ]
-		then
-			break
-		else
-			if [ $1 == "-s" ]
-			then
-				shift
-				if [ $1 -ge 1 ] && [ $1 -le 255 ]
-				then
-					start_ip=$1
-				else
-					help
-					exit
-				fi
-			elif [ $1 == "-e" ]
-			then
-				shift
-				if [ $1 -ge 1 ] && [ $1 -le 255 ]
-				then
-					end_ip=$1
-				else
-					help
-					exit
-				fi
-			elif [ $1 == "-f" ]
-			then
-				shift
-				filter_file=$1
-			fi
-			shift
-		fi
-	done
-	
-	# Check if starting and ending IP's were set
-	if test -z $start_ip
-	then
-		start_ip=0
-	fi
-	if test -z $end_ip
-	then
-		end_ip=255
-	fi
-	
-	# Verify if there are additional arguments for NMAP
-	while true
-	then
-		if [ $# -eq 0 ]
-		then
-			break
-		else
-			if test -z "$args"
-			then
-				args=$1
-			else
-				args="$args $1"
-			fi
-			shift
-		fi
-	done
-}
- 
 # Check if NMAP exists and create a log folder
 nmap-check()
 {
@@ -156,72 +77,160 @@ nmap-check()
 
 do-nmap()
 {
-	nmap -v -n -Pn -sS -sV -p $port "$ip_range"."$ip" $args | tee -a $FOLDER/log_$DATE.txt
-} 
+	echo "nmap -v -n -Pn -sS -sV -p $port $ip_range.$ip $args | tee -a $FOLDER/log_$DATE.txt"
+	nmap -v -n -Pn -sS -sV -p $port $ip_range.$ip $args | tee -a $FOLDER/log_$DATE.txt
+}
 
 nmap-no-filters()
 {
     for port in {1..65535}
     do
-        for ip in {$start_ip..$end_ip}
+        for ip in $(seq $start_ip $end_ip)
         do
             do-nmap
         done
     done
 }
- 
+
 nmap-filtered()
 {
-    ignore=""
-    for port in {1..65535}
-    do
-        for ip in {$start_ip..$end_ip}
-        do
-            ignore="false"
-            for reject in filter_file
-            do
-                if [[ $reject == $ip ]]
-                then
-                    $ignore="true"
-                    break
-                fi
-            done
- 
-            if [[ $ignore == "true" ]]
-            then
-                $ignore = "false"
-                break
-            else
-                do-nmap
-            fi
-        done
-    done
+	# This is a boolean flag to skip NMAP on certain IP addresses
+	ignore=""
+
+	# Loop through ports
+	for port in {1..65535}
+	do
+		# Loop through IP addresses
+		for ip in $(seq $start_ip $end_ip)
+		do
+			ignore="false"
+			for reject in $(cat $filter_file)
+			do
+				# Is the current IP to be rejected?
+				if [[ $reject == $ip_range.$ip ]]
+				then
+					ignore="true"
+					break
+				fi
+			done
+
+		if [[ $ignore == "true" ]]
+		then
+			# If current IP to be reject, loop to next IP
+			continue
+		else
+			# Otherwise, NMAP it!
+			do-nmap
+		fi
+		done
+	done
 }
- 
- 
- 
+
+
+
 # MAIN
- 
- 
- 
-# Check if input was correct 
-check-arguments
- 
+
+
+# If no inputs, help user to use it :)
+if [ $# -eq 0 ]
+then
+	help
+	exit
+fi
+
+# Save first argument and test it
+ip_range=$1
+shift
+sanitize
+if [ $# -ne 0 ]
+then
+	# Verify if there are extra arguments
+	for i in {1..3}
+	do
+		if [ $# -eq 0 ]
+		then
+			break
+		else
+			if [ $1 == "-s" ]
+			then
+				shift
+				if [ $1 -ge 1 ] && [ $1 -le 255 ]
+				then
+					start_ip=$1
+					shift
+				else
+					help
+					exit
+				fi
+			elif [ $1 == "-e" ]
+			then
+				shift
+				if [ $1 -ge 1 ] && [ $1 -le 255 ]
+				then
+					end_ip=$1
+					shift
+				else
+					help
+					exit
+				fi
+			elif [ $1 == "-f" ]
+			then
+				shift
+				filter_file=$1
+				shift
+			fi
+		fi
+	done
+fi
+
+# Check if starting and ending IP's were set
+if test -z $start_ip
+then
+	start_ip=0
+fi
+if test -z $end_ip
+then
+	end_ip=255
+fi
+if [ $start_ip -gt $end_ip ]
+then
+	help
+	exit
+fi
+
+# Verify if there are additional arguments for NMAP
+while true
+do
+	if [ $# -eq 0 ]
+	then
+		break
+	else
+		if test -z "$args"
+		then
+			args=$1
+		else
+			args="$args $1"
+		fi
+		shift
+	fi
+done
+
 # Check if NMAP exists
 nmap-check
- 
-# Sanitize first input
-sanitize
- 
+
+# Just some credits. Feel free to remove this
+echo "Made by dev.cmsantos (dev.cmsantos@gmail.com)"
+echo "MIT License, use at your own risk."
+
 # Is there a filter? Horizontal NMAP will execute differently then.
-if test -f $filter_file
+if [ -e "$filter_file" ] && [ -s $filter_file ]
 then
-    nmap-filtered
+	nmap-filtered
 else
-	echo "Filter file does not exist. Executing non-filtered NMAP"
-    nmap-no-filters
+	echo "Filter file does not exist or is empty. Executing non-filtered NMAP"
+	nmap-no-filters
 fi
- 
+
 echo ""
-echo "Results are in log_$DATE.txt"
+echo "Results are in $FOLDER/log_$DATE.txt"
 echo ""
