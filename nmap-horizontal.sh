589ca0608504e5dc1f629d66b146af43a94756ab
César Santos <cesar-santos@users.noreply.github.com>
2019-05-07 23:04:44 -0300

    Adding port selection file. Fixing for loop bugs

--- nmap-horizontal.sh
+++ nmap-horizontal.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
 # Constants
+FIRST_IP=1
+LAST_IP=255
+FIRST_PORT=1
+LAST_PORT=65535
+FILTER_FILE="/tmp/ports.txt"
+LOG_FOLDER="nmap_logs"
 DATE=$(date +%s)
-FOLDER="nmap_logs"
 
 # Arguments
 ip_range=""
 start_ip=""
 end_ip=""
+filter_ports=""
 filter_file=""
 args=""
 
 # Help function
 help()
 {
-    echo "Made by dev.cmsantos (dev.cmsantos@gmail.com)"
-    echo "MIT License, use at your own risk."
     echo ""
-    echo "    nmap-horizontal ip [-efs] [args]"
+    echo "    nmap-horizontal ip [-efsp] [args]"
     echo "        ip is for a range of IPs, so for instance 192.168.10, then this script will hover over from 1 to 255."
     echo ""
-	echo "        middle argument is to include defined ranges, if you wish to start, let's say, from 30 to 100. Then, you'd type -s 30 -e 100. -f if you want to filter undesired IP address, like gateways (let's say 192.168.10.2 or 192.168.10.255). For such, it will look for a filter file on this very folder, and on each line it will look for IP addresses that won't be checked by this tool."
+	echo "        middle arguments are optional. They are meant to include defined ranges, if you wish to start, let's say, from 30 to 100, or on ports 80 and 445. Then, you'd type -s 30 -e 100 -p PORTS.txt, considering PORTS.txt file will contain lines \"80\" and \"445\". -f if you want to filter undesired IP address, like gateways (let's say 192.168.10.2 or 192.168.10.255). For such, it will look for a filter file on this very folder, and on each line it will look for IP addresses that won't be checked by this tool."
 	echo ""
-    echo "        args is optional. It is for anything extra you want NMAP to do like \"script smb-os-discovery script-args vulns.short\""
+    echo "        args is optional too. It is for anything extra you want NMAP to do like \"script smb-os-discovery script-args vulns.short\""
     echo ""
-	echo "    EXAMPLE: nmap-horizontal 10.20.30 -s 50 -e 70 -f nmap-filter.txt script smb-os-discovery script-args vulns.short"
+	echo "    EXAMPLE: nmap-horizontal 10.20.30 -s 50 -e 70 -f nmap-filter.txt -p ports.txt script smb-os-discovery script-args vulns.short"
 	echo ""
 }
 
@@ -35,11 +39,11 @@ sanitize()
     oct2=$(echo $ip_range | tr "." " " | awk '{print $2}')
     oct3=$(echo $ip_range | tr "." " " | awk '{print $3}')
 
-    if [[ $oct1 -gt 0 && $oct1 -lt 256 ]]
+    if [[ $oct1 -ge $FIRST_IP && $oct1 -le $LAST_IP ]]
     then
-        if [[ $oct2 -gt 0 && $oct2 -lt 256 ]]
+        if [[ $oct2 -ge $FIRST_IP && $oct2 -le $LAST_IP ]]
         then
-            if [[ $oct3 -gt 0 && $oct3 -lt 256 ]]
+            if [[ $oct3 -ge $FIRST_IP && $oct3 -le $LAST_IP ]]
             then
                 : # Do nothing
             else
@@ -66,25 +70,29 @@ nmap-check()
         echo ""
         exit
     else
-        if test -d $FOLDER
+        if test -d $LOG_FOLDER
         then
             : # Do nothing
         else
-            mkdir $FOLDER
+            mkdir $LOG_FOLDER
         fi
     fi
 }
 
 do-nmap()
 {
-	echo "nmap -v -n -Pn -sS -sV -p $port $ip_range.$ip $args | tee -a $FOLDER/log_$DATE.txt"
-	nmap -v -n -Pn -sS -sV -p $port $ip_range.$ip $args | tee -a $FOLDER/log_$DATE.txt
+	echo "nmap -v -n -Pn -sS -sV -p $port $ip_range.$ip $args | tee -a $LOG_FOLDER/log_$DATE.txt"
+	nmap -v -n -Pn -sS -sV -p $port $ip_range.$ip $args | tee -a $LOG_FOLDER/log_$DATE.txt
 }
 
 nmap-no-filters()
 {
-    for port in {1..65535}
+    echo $filter_ports
+	read
+    for port in `cat $filter_ports`
     do
+		echo $port
+		read
         for ip in $(seq $start_ip $end_ip)
         do
             do-nmap
@@ -98,7 +106,7 @@ nmap-filtered()
 	ignore=""
 
 	# Loop through ports
-	for port in {1..65535}
+	for port in `cat $filter_ports`
 	do
 		# Loop through IP addresses
 		for ip in $(seq $start_ip $end_ip)
@@ -131,12 +139,21 @@ nmap-filtered()
 # MAIN
 
 
+# Just some credits. Feel free to remove this
+echo "Made by dev.cmsantos (dev.cmsantos@gmail.com)"
+echo "MIT License, use at your own risk."
+echo -n "Configuring environment"
+
+# Remove old garbage
+rm $filter_ports 2>/dev/null
+
 # If no inputs, help user to use it :)
 if [ $# -eq 0 ]
 then
 	help
 	exit
 fi
+echo -n "."
 
 # Save first argument and test it
 ip_range=$1
@@ -145,7 +162,7 @@ sanitize
 if [ $# -ne 0 ]
 then
 	# Verify if there are extra arguments
-	for i in {1..3}
+	for i in $(seq 1 4)
 	do
 		if [ $# -eq 0 ]
 		then
@@ -154,7 +171,7 @@ then
 			if [ $1 == "-s" ]
 			then
 				shift
-				if [ $1 -ge 1 ] && [ $1 -le 255 ]
+				if [ $1 -ge $FIRST_IP ] && [ $1 -le $LAST_IP ]
 				then
 					start_ip=$1
 					shift
@@ -165,7 +182,7 @@ then
 			elif [ $1 == "-e" ]
 			then
 				shift
-				if [ $1 -ge 1 ] && [ $1 -le 255 ]
+				if [ $1 -ge $FIRST_IP ] && [ $1 -le $LAST_IP ]
 				then
 					end_ip=$1
 					shift
@@ -178,25 +195,46 @@ then
 				shift
 				filter_file=$1
 				shift
+			elif [ $1 == "-p" ]
+			then
+				shift
+				filter_ports=$1
+				shift
 			fi
 		fi
 	done
+	
+	# Make sure IP addresses are possible
+	if [ $start_ip -gt $end_ip ]
+	then
+		help
+		exit
+	fi
 fi
+echo -n "."
 
 # Check if starting and ending IP's were set
 if test -z $start_ip
 then
-	start_ip=0
+	start_ip=$FIRST_IP
 fi
 if test -z $end_ip
 then
-	end_ip=255
+	end_ip=$LAST_IP
 fi
-if [ $start_ip -gt $end_ip ]
+echo -n "."
+
+# Check if a port list will be used
+if test -z $filter_ports
 then
-	help
-	exit
+	filter_ports=$FILTER_FILE
+	touch $filter_ports
+	for i in $(seq $FIRST_PORT $LAST_PORT)
+	do
+		echo $i >> $filter_ports
+	done
 fi
+echo -n "."
 
 # Verify if there are additional arguments for NMAP
 while true
@@ -214,15 +252,14 @@ do
 		shift
 	fi
 done
+echo -n "."
 
 # Check if NMAP exists
 nmap-check
-
-# Just some credits. Feel free to remove this
-echo "Made by dev.cmsantos (dev.cmsantos@gmail.com)"
-echo "MIT License, use at your own risk."
+echo -n "."
 
 # Is there a filter? Horizontal NMAP will execute differently then.
+echo ""
 if [ -e "$filter_file" ] && [ -s $filter_file ]
 then
 	nmap-filtered
@@ -230,7 +267,8 @@ else
 	echo "Filter file does not exist or is empty. Executing non-filtered NMAP"
 	nmap-no-filters
 fi
+rm $filter_ports
 
 echo ""
-echo "Results are in $FOLDER/log_$DATE.txt"
+echo "Results are in $LOG_FOLDER/log_$DATE.txt"
 echo ""
